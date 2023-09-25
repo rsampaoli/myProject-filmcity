17c76c0283c7ba8ef14f7bdb1dee75cbd0830576
ramirosampaoli <ramirosampaoli@gmail.com>
2023-09-25 15:14:50 -0300

    bug en log de usuario (falta proceso)

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+const session = require('express-session');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -21,6 +22,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'Secreto',
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.use('/', indexRouter);
 app.use('/peliculas', peliculaRouter);
@@ -28,12 +34,12 @@ app.use('/usuario', userRouter);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- controllers/userController.js
+++ controllers/userController.js
@@ -17,12 +17,22 @@ const userController = {
             where: {
                 email: req.body.email
             }
+
         });
+        console.log(userFound)
         if (!userFound) { // validar si el usuario no existe en la base de datos
             return res.send('no se encuentra el usuario');
         } else {
             if (req.body.pass === userFound.password) {
-                res.send('inicio correcto')
+                req.session.usuarioLogueado = {
+                    id: userFound.id,
+                    first_name: userFound.first_name,
+                    last_name: userFound.last_name,
+                    email: userFound.email,
+                    avatar_id: userFound.avatar_id,
+                    alias: userFound.alias
+                }
+                res.redirect('/')
             };
         }
         res.send('inicio incorrecto')
