225a94d3b532b1ea6e6abec8728c940a2c045f02
ramirosampaoli <ramirosampaoli@gmail.com>
2023-10-06 12:10:11 -0300

    crear pelicula router

--- routes/movies.js
+++ routes/movies.js
@@ -7,12 +7,12 @@ const upload = require('../utils/multer');
 
 router.get('/listado', moviesController.list);
 router.get('/listado/id/:id', moviesController.detail);
-router.get('/crearPelicula', moviesController.add);
+router.get('/crear', moviesController.add);
 router.get('/editar/id/:id', moviesController.edit);
 router.get('/borrar/id/:id', moviesController.delete)
 
 
-router.post('/crearPelicula', upload.single('image'), moviesController.create);
+router.post('/crear', upload.single('image'), moviesController.create);
 router.post('/editar/id/:id', upload.single('image'), moviesController.update);
 router.post('/borrar/id/:id', moviesController.destroy);
 
